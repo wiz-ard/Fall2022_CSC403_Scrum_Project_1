f40a86e57791a3cfef1232026128b6ba190fa1ce
Family-123 <62144602+Family-123@users.noreply.github.com>
2022-11-16 11:54:28 -0600

    Update FrmBattle.Designer.cs

--- Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
@@ -89,7 +89,7 @@
       this.lblPlayerMagicFull.BackColor = System.Drawing.Color.Blue;
       this.lblPlayerMagicFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.lblPlayerMagicFull.ForeColor = System.Drawing.Color.White;
-      this.lblPlayerMagicFull.Location = new System.Drawing.Point(71, 60);
+      this.lblPlayerMagicFull.Location = new System.Drawing.Point(71, 30);
       this.lblPlayerMagicFull.Name = "lblPlayerMagicFull";
       this.lblPlayerMagicFull.Size = new System.Drawing.Size(226, 20);
       this.lblPlayerMagicFull.TabIndex = 3;
