253a7e721aaeb93c0f201310cf4e559a47883204
Family-123 <62144602+Family-123@users.noreply.github.com>
2022-11-16 13:01:14 -0600

    Add files via upload

---
+++ Project/MyGameLibrary/HardGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code {
+  public static class HardGame {
+    public static HardPlayer hardplayer = null;
+
+  }
+}
